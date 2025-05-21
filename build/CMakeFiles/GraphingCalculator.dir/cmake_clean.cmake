ea66414df5e78abda52ef0c9608600abf3eb9fd9
Norr <norayr.avetisyan.6@gmail.com>
2025-05-22 00:55:47 +0400

    add imgui lib, and cmake

---
+++ build/CMakeFiles/GraphingCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphingCalculator.dir/lib/glad/src/glad.c.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/glad/src/glad.c.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/lib/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/GraphingCalculator.dir/test_main.cpp.o"
+  "CMakeFiles/GraphingCalculator.dir/test_main.cpp.o.d"
+  "GraphingCalculator"
+  "GraphingCalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GraphingCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_main.cpp
@@ -0,0 +1,60 @@
+
+#include "../lib/imgui/imgui.h"
+#include "../lib/imgui/backends/imgui_impl_glfw.h"
+#include "../lib/imgui/backends/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main() {
+    if (!glfwInit()) return -1;
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui + OpenGL", NULL, NULL);
+    if (!window) return -1;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Example window
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("This is Dear ImGui + OpenGL!");
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
